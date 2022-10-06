475a0226c940d7a4e92b1e75031dff7f9f2ce83a
atanerortac <atanerortac48@gmail.com>
2022-10-06 15:00:05 +0300

    Login page icon added.

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,11 +8,7 @@ interface Props {
 const Login = ({ providers }: Props) => {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img
-        className="w-52 mb-5"
-        src="https://links.papareact.com/9xl"
-        alt="spoIcon"
-      />
+      <img className="w-52 mb-5" src="/spotify-icon.png" alt="spoIcon" />
       {Object.values(providers).map((provider: any) => (
         <div key={provider.name}>
           <button
